59180939db9b78388ed0e09c64ed99dc99fa009e
shivkumar98 <shiv_kumar.98@outlook.com>
2020-06-25 10:48:02 +0100

    first commit

---
+++ FileFileInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"c:\temp\myfile.jpg";
+            File.Copy(path, @"d:\temp\myfile.jpg",true);
+            if (File.Exists(path))
+            {
+                Console.WriteLine("hello");
+            }
+            var content = File.ReadAllText(path);
+            Console.WriteLine(content);
+
+            var fileInfo = new FileInfo(path);
+            Console.WriteLine(fileInfo);
+
+
+         
+            if (fileInfo.Exists)
+            {
+                fileInfo.Delete();
+            }
+
+
+        }
+    }
+}
